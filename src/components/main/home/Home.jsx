7a59b801b53ac8007b1e11db669864ac3e68beed
TimWang95 <tgm761@gmail.com>
2023-07-13 18:27:23 +0800

    add home layout

--- src/components/main/home/Home.jsx
+++ src/components/main/home/Home.jsx
@@ -1,7 +1,18 @@
+import bgMobile from 'src/assets/home/background-home-mobile.jpg'
+
 function Home() {
   return (
     <>
-    
+      <div className="home">
+        <div className="content-container">
+          <h5>SO, YOU WANT TO TRAVEL TO</h5>
+          <h1>SPACE</h1>
+          <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+        </div>
+        <div className="btn-container">
+          <button>EXPLORE</button>
+        </div>
+      </div>
     </>
   )
 }
