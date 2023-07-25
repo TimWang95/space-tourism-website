ec3294adc14b5573e3d028f2208eae57824dfca0
TimWang95 <tgm761@gmail.com>
2023-07-25 19:39:16 +0800

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { Header, Home, Destination, Crew, Technology } from 'src/components/index'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header, Home, Destination, Crew, Technology } from 'src/components/index';
 import 'src/styles/index.scss';
+
+import data from 'src/data/data.json'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
-      <Destination/>
-      <Crew/>
-      <Technology/>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="*" element={<Home/>}/>
+          <Route path="/destination" element={<Destination data={data.destinations} />}/>
+          <Route path="/crew" element={<Crew data={data.crew} />}/>
+          <Route path="/technology" element={<Technology data={data.technology} />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 
 export default App;
